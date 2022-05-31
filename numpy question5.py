ccb632c0b950783c035619652c8596403114fe3f
saurabhagra <106605122+saurabhagra@users.noreply.github.com>
2022-05-31 20:35:29 +0530

    Create numpy question5.py

---
+++ numpy question5.py
@@ -0,0 +1,9 @@
+# Find the most frequent value in a NumPy array
+import numpy as np
+
+x = np.array([1,2,3,4,5,1,2,1,1,1])
+print("Original array:")
+print(x)
+
+print("Most frequent value in the above array:")
+print(np.bincount(x).argmax())
